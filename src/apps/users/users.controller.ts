1a2a4bfe844340a95c94a3a9fa8930759d66e3bc
hengage <henrychizobaudeh@gmail.com>
2025-12-25 11:14:04 +0100

    chore(swagger): add api response dto

--- src/apps/users/users.controller.ts
+++ src/apps/users/users.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponseDto } from 'src/common/dtos/api-response.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiResponse({ type: () => ApiResponseDto<User> })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
